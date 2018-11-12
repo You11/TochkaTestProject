788948c5531cbe0a9e36d79664cb9b774b0431d0
aa shulpin <aashulpin@gmail.com>
2018-11-12 09:34:03 +0500

    temporary fixed error on search on android 4.0-4.4 devices

--- app/src/main/java/ru/you11/tochkatestproject/main/SearchPresenter.kt
+++ app/src/main/java/ru/you11/tochkatestproject/main/SearchPresenter.kt
@@ -1,6 +1,10 @@
 package ru.you11.tochkatestproject.main
 
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.security.ProviderInstaller
 import io.reactivex.Completable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -11,10 +15,10 @@ import ru.you11.tochkatestproject.model.GithubUser
 import java.util.*
 import java.util.concurrent.TimeUnit
 
-class SearchPresenter(private val searchView: SearchFragment): MainContract.SearchContract.Presenter {
+class SearchPresenter(private val searchFragment: SearchFragment): MainContract.SearchContract.Presenter {
 
     init {
-        searchView.presenter = this
+        searchFragment.presenter = this
     }
 
     private val DELAY_TIME_MS: Long = 600
@@ -23,7 +27,7 @@ class SearchPresenter(private val searchView: SearchFragment): MainContract.Sear
     private lateinit var onInputDisposable: Disposable
 
     override fun start() {
-        searchView.showStartingScreen()
+        searchFragment.showStartingScreen()
     }
 
     override fun loadWithDelay(query: String) {
@@ -40,6 +44,7 @@ class SearchPresenter(private val searchView: SearchFragment): MainContract.Sear
 
     override fun loadGithubUsers(query: String, page: Int) {
         val githubService = GithubService.create()
+        updateAndroidSecurityProvider()
         compDisposable.add(githubService.usersSearch(query, page)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
@@ -47,13 +52,25 @@ class SearchPresenter(private val searchView: SearchFragment): MainContract.Sear
                 val userArrayList = getArrayListFromClass(result.users)
                 val numberOfPages = getNumberOfPages(result.usersCount)
                 if (numberOfPages == 0) {
-                    searchView.showNoGithubUsersFoundScreen()
+                    searchFragment.showNoGithubUsersFoundScreen()
                 } else {
-                    searchView.showGithubUsersPage(userArrayList, page, numberOfPages)
+                    searchFragment.showGithubUsersPage(userArrayList, page, numberOfPages)
                 }
             }, { error ->
-                searchView.showLoadingGithubUsersError(error)
+                searchFragment.showLoadingGithubUsersError(error)
             }))
+    }
+
+    private fun updateAndroidSecurityProvider() {
+        try {
+            ProviderInstaller.installIfNeeded(searchFragment.activity)
+        } catch (exception: GooglePlayServicesRepairableException) {
+            // Thrown when Google Play Services is not installed, up-to-date, or enabled
+            // Show dialog to allow users to install, update, or otherwise enable Google Play services.
+            Log.e("SecurityException", exception.localizedMessage)
+        } catch (exception: GooglePlayServicesNotAvailableException) {
+            Log.e("SecurityException", "Google Play Services not available.")
+        }
     }
 
     private fun getArrayListFromClass(users: List<GithubUser>): ArrayList<GithubUser> {
